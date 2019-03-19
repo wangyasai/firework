3450b9f010cb6d2996fac3f8977965d5ea8774d1
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2019-03-19 20:57:42 +0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -17,6 +17,7 @@ let firework18;
 var time;
 var s;
 var a = 0;
+var alpha = 255;
 
 function preload(){
   data = loadTable("data/data.csv", 'csv');
@@ -52,8 +53,7 @@ function setup(){
     fireworks[j] = new Firework(posX[j],posY[j],y[j],0);
   }
   oneFirework = new Firework(width/2, bottom,y[1],0);
-  firework17 = new Firework(width/2, y[1],y[1],0);
-  firework18 = new Firework(width/2, y[2],y[2],0);
+  firework18 = new Firework(width/2,  bottom,y[2]+100,0);
 }
 
 
@@ -75,7 +75,6 @@ function mousePressed(){
   if(a==0){
     s = frameCount; 
   }
-  
   a = 1; 
 }
 
@@ -87,47 +86,68 @@ function intro(){
     stroke(20);
     line(left,i,right,i);
   } 
-  fill(255);
+
+  var angle = map(frameCount,100,150,PI*1.5,PI*3.5);
+  noFill();
+  stroke(255);
+  strokeWeight(1);
+  if(frameCount>100&&frameCount<150){ 
+    arc(width/2, height/3,120,120,PI*1.5, angle); 
+  }else if(frameCount>=150){
+    ellipse(width/2, height/3,120,120); 
+  }
+
+  fill(255);  
+  noStroke();
+  textSize(12);
+  var rr = 90;
+  if(frameCount>100){
+    text("除夕中午12点",width/2,height/3-rr);
+  }
+  if(frameCount>110){
+
+    text("除夕下午6点",width/2+rr*1.3,height/3+5);
+  }
+  if(frameCount>120){
+    text("大年初一0点",width/2,height/3+rr);
+  }
+  if(frameCount>130){
+    line(width/2+rr+0,height/3,width/2+rr-10,height/3);
+    text("大年初一中午12点",width/2-rr*1.4,height/3+5);
+  }
+
+  for(var i =0; i<4;i++){
+    if(frameCount>100+i*10){
+      push();
+      translate(width/2,height/3);
+      rotate(PI+i*PI/2);
+      stroke(255);
+      strokeWeight(1);
+      line(0,60,0,70);
+      pop();
+    }
+  }
+
+  fill(254,82,105,this.alpha);
   oneFirework.display(1);
   oneFirework.move();
   oneFirework.city(1);
 
-  var angle = map(frameCount,70,120,PI*1.5,PI*3.5);
-  noFill();
-  stroke(255);
-  strokeWeight(1);
-  if(frameCount>70&&frameCount<120){ 
-    arc(width/2, height/3,120,120,PI*1.5, angle); 
-  }else if(frameCount>120){
-    ellipse(width/2, height/3,120,120); 
-  }
-
-  fill(255);
-  noStroke();
-  textSize(12);
-  var rr = 90;
-  if(frameCount>70){
-    text("除夕中午12点",width/2,height/3-rr);
-  }
-  if(frameCount>80){
-    text("除夕下午6点",width/2+rr*1.3,height/3);
-  }
-  if(frameCount>90){
-    text("除夕下午6点",width/2,height/3+rr);
-  }
-  if(frameCount>100){
-    text("除夕下午6点",width/2-rr*1.3,height/3);
-  }
-
-  if(frameCount>110){
-     fill(255,222,85,this.alpha);//黄色 2018
-     firework17.display(1);
-     fill(254,82,105,this.alpha);//红色 2017
-     firework18.display(2);
-   }
+  if(frameCount>200){
+   fill(255,222,85,this.alpha);
+   firework18.display(2);
+   firework18.move();
+   text("2018年数据",width/2+100,y[2]+105);
  }
 
- function fireworkStart(){
+  if(frameCount>300){
+    fill(255,510*sin(frameCount/10));
+    text("点击屏幕看城市烟花秀",width/2,height*0.7);
+  } 
+}
+
+
+function fireworkStart(){
   time = frameCount-s;
 
   //dot line
@@ -148,41 +168,11 @@ function intro(){
   for(var j = 1; j < cols; j++){
     if(j<= time){ 
       fireworks[j].move();
-      if (j%2==0) {
-       fill(255,222,85,this.alpha);//黄色 2018
-     } else if (j%2==1) {
-       fill(254,82,105,this.alpha);//红色 2017
-     }
-     fireworks[j].display(j);
-     fireworks[j].city(j);     
-   }
- }
-}
-
-
-function first(){
-  for(var i = 2; i < rows; i++ ){
-    push();
-    translate(width/2, height/3);
-    var r = map(data.getNum(i,1), 0, 300, 0, 80);
-    var offset = map(data.getNum(i,1),0,300,0,3);  
-    fill(255,222,85);
-    noStroke();
-    rotate(-PI-TWO_PI/24*(i-2)); 
-    beginShape();  
-    if(frameCount > 100+(25-i)) {
-      vertex(0,0);
-      vertex(-offset ,r);
-      vertex(offset ,r);
-      endShape(CLOSE);
-      ellipse(0,r,offset*2);
+      fireworks[j].display(j);
+      fireworks[j].city(j);
+      fireworks[j].infor(j);     
     }
-    endShape(CLOSE);
-    pop();     
-
-    noFill();
-    stroke(255);
-  }    
+  }
 }
 
 
@@ -194,7 +184,7 @@ class Firework{
     this.easing = 0.05;
     this.w = 80;
     this.r = r; 
-    this.alpha = -100;  
+    this.alpha = 255;  
   }
 
 
@@ -203,7 +193,7 @@ class Firework{
       var r = map(data.getNum(i,j), 0, 300, 0, this.w);
       push();
       translate(this.x,this.y);     
-      this.alpha += (255-this.alpha)*this.easing;   
+      // this.alpha += (255-this.alpha)*this.easing;   
 
       noStroke();
 
@@ -216,43 +206,87 @@ class Firework{
      }
      this.r += (data.getNum(i,j)/n-this.r)*this.easing/2;
      var offset = map(data.getNum(i,j),0,300,0,3);
-
+     if ((j)%2==0) {
+       fill(255,222,85,this.alpha);//黄色 2018
+     } else if ((j)%2==1) {
+       fill(254,82,105,this.alpha);//红色 2017
+     }
      beginShape();
      vertex(0,0);
      vertex(-offset ,r/n);
      vertex(offset ,r/n);
      endShape(CLOSE);
-     ellipse(0,r/n,offset*2);
-     pop();        
-   }    
+     arc(0,r/n,offset*2,offset*2,PI*2,PI*3);
+       // ellipse(0,r/n,offset*2);
+       pop();        
+     }    
+   }
 
- }
 
- move(){
-  this.y += (this.tY- this.y)*this.easing;
-}
-
-city(j){
-  fill(220);
-  noStroke();
-  var n_;
-  if(width>1000){
-   n_= 16;
- }else{
-   n_ = 12;
- }
- textSize(n_);
- textAlign(CENTER);
- for (var i = 0; i < data.getString(0, j).length; i++) { 
-  if (j%2==1) {
-    text(data.getString(0, j).charAt(i), this.x, bottom+25+(n_+3)*i);
+   move(){
+    this.y += (this.tY- this.y)*this.easing;
   }
+
+  //city name
+  city(j){
+    fill(220);
+    noStroke();
+    var n_;
+    if(width>1000){
+      n_= 16;
+    }else{
+      n_ = 12;
+    }
+    textSize(n_);
+    textAlign(CENTER);
+    for (var i = 0; i < data.getString(0, j).length; i++) { 
+      if (j%2==1) {
+        text(data.getString(0, j).charAt(i), this.x, bottom+25+(n_+3)*i);
+      }
+    }
+  }
+
+
+  // city connect firework 
+  lines(){
+    stroke(100);
+    strokeWeight(1);
+    line(this.x,this.y,this.x,bottom);
+  }
+
+
+  //show 2017 / 2018
+  infor(j){
+   fill(255,222,85);
+   ellipse(width*0.8,height*0.1,20,20)
+   fill(254,82,105);
+   ellipse(width*0.8+30,height*0.1,20,20)
+   if(dist(mouseX,mouseY,width*0.8,height*0.1)<10){
+    if ((j)%2==0) {
+     this.alpha = 255;
+   } else if ((j)%2==1) {
+    this.alpha = 50;
+  }
+}else if(dist(mouseX,mouseY,width*0.8+30,height*0.1)<10){
+ if ((j)%2==0) {
+   this.alpha = 50;
+ } else if ((j)%2==1) {
+  this.alpha = 255;
+}
+}else{
+  this.alpha = 255;
+}
 }
 }
 
-lines(){
-  stroke(100);
-  strokeWeight(1);
-  line(this.x,this.y,this.x,bottom);
-}
-}
+
+
+
+
+
+
+
+
+
+
+
