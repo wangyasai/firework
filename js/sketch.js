6d81c94c85cb88b6bd0b2a7dbd5ebdfcc20eb0a4
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2019-04-04 22:52:21 +0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,28 @@
 
 var rows;
 var cols;
-var data;
+var data17,data8,data19;
 var posX = [];
-var y = [];
+var y17 = [];
+var y18 = [];
+var y19 = [];
 var posY = [];
-var sum = [];
-var avg = [];
+var sum17 = [];
+var sum18 = [];
+var sum19 = [];
+var avg17 = [];
+var avg18 = [];
+var avg19 = [];
 var left,right,above,bottom;
 var s;
-let fireworks = [];
+let fireworks17 = [];
+let fireworks18 = [];
+let fireworks19 = [];
 let r = [];
 let oneFirework;
 let firework17;
 let firework18;
+
 var time;
 var s;
 var a = 0;
@@ -22,8 +31,77 @@ var alpha = 255;
 var alpha = [];
 var viewW,viewH;
 
+var policy =["升级为禁放","升级为禁放","扩大禁放范围","维持禁放","升级到或维持禁放","无禁限措施"];
+var policyS = [1,1,12,21,34,43];
+var policyE = [11,11,20,33,42,50];
+
+var rank = -1;
+var index = 1;
+//中转值
+var temp17 = [];
+var temp18 = [];
+var temp19 = [];
+var btW = 45;
+var btH = 20;
+
+var targetX17 = [];
+var targetX18 = [];
+var targetX19 = [];
+
+var startLx =[];
+var lx =[];
+var targetLx = [];
+var alpha18,alpha17 = 255;
+
+
+var hlcolor = (255,0,74);
+var unhlcolor = (220);
+
+var hltxt = 250;
+var unhltxt = 100;
+
+
+
+var txtBlack  = 20;
+var txtWhite = 255;
+var nullColor = 33;
+var yellowColor = '#FFDE55';
+var redColor ='#FE5269';
+var goldColor = '#F2D5AB';
+
+var btnYear17 = '#FE5269';
+var btnYear18 = '#FFDE55';
+var btnRankAll = '#F2D5AB';
+var btnRank17 = nullColor;
+var btnRank18 = nullColor;
+var btnCompare = nullColor;
+
+
+
+var txtYear17 = txtBlack;
+var txtYear18 = txtBlack;
+var txtRankAll = txtBlack;
+var txtRank17 = txtWhite;
+var txtRank18 = txtWhite;
+var txtCompare = txtWhite;
+
+
+var button = "a";
+var btnY = 80;
+var btn1, btn2, btn3; //按钮
+var btnW = 60;
+var btnH = 30;
+
+var count17 = 1;
+var count18 = 1;
+
+var logo;
+
 function preload(){
-  data = loadTable("data/data.csv", 'csv');
+  data17 = loadTable("data/2017.csv", 'csv');
+  data18 = loadTable("data/2018.csv", 'csv');
+  data19 = loadTable("data/2019.csv", 'csv');
+  logo = loadImage("images/logo.png","png");
 }
 
 
@@ -36,9 +114,8 @@ function setup(){
     viewW = windowWidth;
   }
   createCanvas(viewW,viewH);
-  background(20,0,100);
-  rows = data.getRowCount();
-  cols = data.getColumnCount();
+  rows = data17.getRowCount();
+  cols = data17.getColumnCount();
 
   if(windowWidth > 1000){
     left = width*0.1;
@@ -47,33 +124,64 @@ function setup(){
     left = width*0.1;
     right = width*0.9;
   }
-  
   above = height*0.2;
   bottom = height*0.85;
 
+
+
+  for(var i = 1 ; i < 6;i++){
+    startLx[i] = map(policyS[i]+1, 1, 50, left, right);
+    targetLx[i]= map(policyE[i]+1, 1, 50, left, right);
+    lx[i] = startLx[i];
+  }
+
+
   for(var j = 1; j < cols; j++){
-    sum[j] = 0;
+    sum17[j] = 0;
+    sum18[j] = 0;   
+    sum19[j] = 0;
     r[j]=[];
+
     for(var i = 3; i < rows; i++){
-      sum[j] += data.getNum(i,j);
+      sum17[j] += data17.getNum(i,j);
+      sum18[j] += data18.getNum(i,j);
+      sum19[j] += data19.getNum(i,j);
       r[j][i]= 0;
     }
-    avg[j] = sum[j]/24;
-    posX[j] = map(int((j+1)/2), 1, 50, left, right);
-    y[j] = map(avg[j], 0, 200, bottom, above);  
+
+    avg17[j] = sum17[j]/24;
+    avg18[j] = sum18[j]/24;
+    avg19[j] = sum19[j]/24;
+    posX[j] = map(int((j+1)), 1, 50, left, right);
+    y17[j] = map(avg17[j], 0, 200, bottom, above);  
+    y18[j] = map(avg18[j], 0, 200, bottom, above); 
+    y19[j] = map(avg19[j], 0, 200, bottom, above);   
     posY[j] = bottom;
     alpha[j] = 255;
-    fireworks[j] = new Firework(posX[j],posY[j],y[j],alpha[j]);
+
+    temp17[j] = avg17[j];
+    temp18[j] = avg18[j];
+    temp19[j] = avg18[j];
+    targetX17[j] = posX[j];
+    targetX18[j] = posX[j];
+    targetX19[j] = posX[j];
+    fireworks17[j] = new Firework(posX[j],posY[j], targetX17[j],y17[j]);
+    fireworks18[j] = new Firework(posX[j],posY[j], targetX18[j],y18[j]);
+    fireworks19[j] = new Firework(posX[j],posY[j], targetX19[j],y19[j]);
 
   }
-  oneFirework = new Firework(width/2, bottom-50,y[1],255);
-  firework18 = new Firework(width/2,  bottom-50,y[2]+100,255);
+  oneFirework = new Firework(width/2, bottom-50,width/2,y17[1]);
+  firework18 = new Firework(width/2,  bottom-50,width/2,y18[1]+100);
+
+  selection(temp17, rank);
+  selection(temp18, rank);
+  selection(temp19, rank);
 }
 
 
 
 function draw(){ 
-  background(20);  
+  background(33);  
   
   if(a==0){
     push();
@@ -84,18 +192,166 @@ function draw(){
     pop();
   }
 
-  if(a==1){
+  if(a==1){ 
+    drawBtn(btnY) ;
     fireworkStart();
+    imageMode(RIGHT);
+     image(logo,right-100,80,120,25);
   }  
+
+ 
+
 }
 
 
-function mousePressed(){
-  if(a==0){
-    s = frameCount; 
+function drawBtn(btnY) {
+  rectMode(CORNER);
+  strokeCap(ROUND);
+  noStroke();
+  fill(255);
+  textAlign(RIGHT);
+  text("显示年份",width/2-5*btnW-4,btnY+btH-5);
+  text("排序",width/2-2*btnW-4,btnY+btH-5);
+  stroke(220);
+  strokeWeight(1);
+
+  fill(btnYear17);
+  rect(width/2-5*btnW, btnY, btnW, btnH);
+  fill(btnYear18);
+  rect(width/2-4*btnW, btnY, btnW, btnH);
+
+  fill(btnRankAll);
+  rect(width/2-2*btnW, btnY, btnW, btnH);
+  fill(btnRank17);
+  rect(width/2-1*btnW, btnY, btnW, btnH);
+  fill(btnRank18);
+  rect(width/2,btnY, btnW, btnH);
+
+  fill(btnCompare);
+  rect(width/2+2*btnW, btnY, btnW, btnH);
+
+  noStroke();
+  textAlign(CENTER, CENTER);
+  textSize(14);
+
+  fill(txtYear17);
+  text("2017", width/2-5*btnW, btnY, btnW, btnH);
+
+  fill(txtYear18);
+  text("2018", width/2-4*btnW, btnY, btnW, btnH);
+
+  fill(txtRankAll);
+  text("政策", width/2-2*btnW, btnY, btnW, btnH);
+
+  fill(txtRank17);
+  text("2017", width/2-1*btnW, btnY, btnW, btnH);
+
+  fill(txtRank18);
+  text("2018", width/2, btnY, btnW, btnH);
+
+  fill(txtCompare);
+  text("对比", width/2+2*btnW, btnY, btnW, btnH);
+}
+
+function mousePressed() {
+ if(a==0){
+  s = frameCount; 
+}
+a =1; 
+if (mouseX > width/2-2*btnW && mouseX< width/2-1*btnW && mouseY > btnY && mouseY< btnY + btnH) {
+  button = "a";
+  count17 = 1;
+  count18 = 1;
+  btnYear17 = redColor;
+  btnYear18 = yellowColor;
+  btnRank17 = nullColor;
+  btnRank18 = nullColor;
+  btnRankAll = goldColor;
+  btnCompare = nullColor;
+
+  txtRankAll = txtBlack;
+  txtYear17 = txtBlack;
+  txtYear18 = txtBlack; 
+  txtRank17 = txtWhite;
+  txtRank18 = txtWhite;
+  txtCompare = txtWhite;
+} else if (mouseX > width/2-1*btnW && mouseX < width/2&&mouseY> btnY && mouseY < btnY + btnH) {
+  button = "b";
+  count17 = 0;
+  btnRank17 = redColor;
+  btnRank18 = nullColor;
+  btnRankAll = nullColor;
+  btnYear17 = redColor;
+  btnYear18 = nullColor;
+  btnCompare = nullColor;
+
+  txtYear17 = txtBlack;
+  txtYear18 = txtWhite;
+  txtRankAll = txtWhite;
+  txtRank17 = txtBlack;
+  txtRank18 = txtWhite;
+  txtCompare = txtWhite;
+} else if (mouseX > width/2&& mouseX < width/2+btnW&&mouseY> btnY && mouseY < btnY + btnH) {
+  button = "c";
+  count18=0;
+  btnRank17 = nullColor;
+  btnRank18 = yellowColor;
+  btnRankAll = nullColor;
+  btnYear17 = nullColor;
+  btnYear18 = yellowColor;
+  btnCompare = nullColor;
+
+
+  txtYear18 = txtBlack;
+
+  txtYear17 = txtWhite;
+  
+  txtRankAll = txtWhite;
+  txtRank17 = txtWhite;
+  txtRank18 = txtBlack;
+  txtCompare = txtWhite;
+} else if (mouseX > width/2+2*btnW && mouseX < width/2+3*btnW&&mouseY> btnY && mouseY < btnY + btnH) {
+  button = "d";
+  btnRank17 = nullColor;
+  btnRank18 = nullColor;
+  btnRankAll = nullColor;
+  btnYear17 = nullColor;
+  btnYear18 = nullColor;
+  btnCompare = goldColor;
+
+  txtYear17 = txtWhite;
+  txtYear18 = txtWhite;
+  txtRankAll = txtWhite;
+  txtRank17 = txtWhite;
+  txtRank18 = txtWhite;
+  txtCompare = txtBlack;
+
+} 
+
+if (mouseX > width/2-5*btnW && mouseX < width/2-4*btnW&&mouseY> btnY && mouseY < btnY + btnH) {
+  count17++;
+  if(count17%2==0){
+    btnYear17 = nullColor;
+    txtYear17 = txtWhite;
+  }else{
+    btnYear17 = redColor;
+    txtYear17 = txtBlack;
   }
-  a =1; 
+}else if (mouseX > width/2-4*btnW && mouseX < width/2-3*btnW&&mouseY> btnY && mouseY < btnY + btnH) {
+  count18++;
+  if(count18%2==0){
+   btnYear18 = nullColor;
+   txtYear18 = txtWhite;
+ }else{
+   btnYear18 = yellowColor;
+   txtYear18 = txtBlack;
+ }
 }
+}
+
+
+
+
 
 
 function intro(){
@@ -104,33 +360,45 @@ function intro(){
   noFill();
   stroke(255);
   strokeWeight(1);
-  if(frameCount>100&&frameCount<150){ 
-    arc(width/2, height/3+60,120,120,PI*1.5, angle); 
-  }else if(frameCount>=150){
+
+  if(frameCount>60 && frameCount<=90){ 
+    arc(width/2, height/3+60,120,120,PI*1.5,angle); 
+  }else if(frameCount>=90){
     ellipse(width/2, height/3+60,120,120); 
   }
+
+
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text("每一根柱子代表每个时间段PM2.5/CO的值",width/2,above);
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+    rect(width/2 -180, above-25,360,35);
 
   fill(255);  
   noStroke();
   textSize(12);
   var rr = 90;
-  if(frameCount>100){
-    textAlign(CENTER);
+
+
+  if(frameCount>60){
     text("除夕中午12点",width/2,height/3+60-rr);
   }
-  if(frameCount>110){
+  if(frameCount>70){
     text("除夕下午6点",width/2+rr*1.3,height/3+60+5);
   }
-  if(frameCount>120){
+  if(frameCount>80){
     text("大年初一0点",width/2,height/3+60+rr);
   }
-  if(frameCount>130){
+  if(frameCount>90){
     line(width/2+rr+0,height/3,width/2+rr-10,height/3+60);
     text("大年初一中午12点",width/2-rr*1.4,height/3+5+60);
   }
 
-  for(var i =0; i<4;i++){
-    if(frameCount>100+i*10){
+  for(var i =0; i<4; i++){
+    if(frameCount>60+i*10){
       push();
       translate(width/2,height/3+60);
       rotate(PI+i*PI/2);
@@ -141,21 +409,25 @@ function intro(){
     }
   }
 
-  oneFirework.display(1);
+  oneFirework.display(1,data17);
   oneFirework.move();
   oneFirework.city(1);
 
-  if(frameCount>200){
-   fill(255,222,85,this.alpha);
-   firework18.display(2);
-   firework18.move();
-   text("2018年数据",width/2+100,y[2]+105);
- }
+  if(frameCount>100){
 
- if(frameCount>300){
-  fill(255,510*sin(frameCount/10));
-  text("点击屏幕看城市烟花秀",width/2,height*0.7);
-}   
+    firework18.display(2,data18);
+    firework18.move();
+    fill(255,222,85);
+    text("2018年数据",width/2+100,y18[2]);
+    fill(254,82,105);
+    text("2017年数据",width/2+100,y18[2]-40);
+
+  }
+
+  if(frameCount>150){
+    fill(255,510*sin(frameCount/10));
+    text("点击屏幕看城市烟花秀",width/2,height*0.7);
+  }    
 }
 
 
@@ -165,7 +437,8 @@ function fireworkStart(){
   //dot line
   for(var j = 1; j < cols; j++){
     if(j <= time){
-      fireworks[j].colLines(j);
+      fireworks17[j].colLines(j);
+      fireworks18[j].colLines(j);
     }
   } 
   
@@ -195,54 +468,74 @@ function fireworkStart(){
   }
 }
 
-  //fireworks
-  for(var j = 1; j < cols; j++){
-    if(j<= time){ 
-      fireworks[j].move();
-      fireworks[j].display(j);   
-      fireworks[j].city(j);
-      fireworks[j].infor(j);   
-    } 
-  }
-  check();
-}
-
-
-class Firework{
-  constructor(posX,posY,targetY,alphas) {
-    this.x = posX;
-    this.y = posY;
-    this.tY = targetY;
-    this.easing = 0.09;
-    this.w = 80;
-    this.r = 0; 
-    this.alpha = alphas; 
+    //fireworks
+    for(var j = 1; j < cols; j++){
+      if(j<= time){ 
+        fireworks17[j].move();
+        fireworks17[j].display(j,data17);   
+        fireworks17[j].city(j);
+        fireworks17[j].infor(j,avg17,data17);   
+        fireworks18[j].move();
+        fireworks18[j].display(j,data18);   
+        fireworks18[j].infor(j,avg18,data18);   
+      } 
+    }
   }
 
+  function policies(){ 
+    for(var i = 1; i<6;i++){
 
-  display(j){  
-    for(var i = 3; i < rows; i++ ){
-      push();
-      translate(this.x,this.y);     
-      noStroke();
+      if(policyS[i]<= time){ 
+        lx[i] += (targetLx[i]-lx[i])*0.09;
+        stroke(241,214,171);
+        strokeWeight(3);
+        strokeCap(SQUARE);
+        line(startLx[i],bottom, lx[i],bottom);
 
-      var n ;
-      rotate(-PI-TWO_PI/24*(i-2));  
-      if(width>1000){
-        n = 1.9;
-      }else{
-        n = 3;
+        fill(241,214,171);
+        noStroke();
+        textAlign(CENTER);
+        text(policy[i],(targetLx[i]-startLx[i])/2+startLx[i],bottom-10)
+      }
+    }
+  }
+
+  class Firework{
+    constructor(posX,posY,targetX,targetY) {
+      this.x = posX;
+      this.y = posY;
+      this.tY = targetY;
+      this.tX = targetX;
+      this.easing = 0.09;
+      this.w = 80;
+      this.r = 0; 
+    }
+
+
+    display(j,data){  
+      for(var i = 3; i < rows; i++ ){
+        push();
+        translate(this.x,this.y);     
+        noStroke();
+
+        var n ;
+        rotate(-PI-TWO_PI/24*(i-2));  
+        if(width>1000){
+          n = 1.9;
+        }else{
+          n = 3;
+        }
+
+        r[j][i]+= (data.getNum(i,j)/n-r[j][i])*this.easing;
+        this.r = r[j][i];
+        var n_ = map(width,0,2000,0,5);
+        var offset = map(data.getNum(i,j),0,300,0,n_);
+        if (data.getNum(1,j)==2018) {
+        fill(255,222,85,alpha18);//黄色 2018
+      } else if (data.getNum(1,j)==2017){
+        fill(254,82,105,alpha17);//红色 2017
       }
 
-      r[j][i]+= (data.getNum(i,j)/n-r[j][i])*this.easing;
-      this.r = r[j][i];
-      var n_ = map(width,0,2000,0,5);
-      var offset = map(data.getNum(i,j),0,300,0,n_);
-      if ((j)%2==0) {
-        fill(255,222,85,this.alpha);//黄色 2018
-      } else if ((j)%2==1) {
-        fill(254,82,105,this.alpha);//红色 2017
-      }
       noStroke();
       if(time>j && a==1){
         beginShape();
@@ -251,7 +544,7 @@ class Firework{
         vertex( offset ,this.r/n);
         endShape(CLOSE);
         arc(0,this.r/n,offset*2,offset*2,PI*2,PI*3);
-      }else if(frameCount >100+ (24-i) && a==0){
+      }else if(frameCount >60+ (24-i) && a==0){
         beginShape();
         vertex(0,0);
         vertex(-offset ,this.r/n);
@@ -266,7 +559,8 @@ class Firework{
 
 
   move(){
-    this.y += (this.tY- this.y)*this.easing;
+    this.y += (this.tY - this.y)*this.easing;
+    this.x += (this.tX - this.x)*this.easing;
   }
 
   //city name
@@ -281,10 +575,8 @@ class Firework{
     }
     textSize(n_);
     textAlign(CENTER);
-    for (var i = 0; i < data.getString(0, j).length; i++) { 
-      if (j%2==1) {
-        text(data.getString(0, j).charAt(i), this.x, bottom+25+(n_+3)*i);
-      }
+    for (var i = 0; i < data17.getString(0, j).length; i++) { 
+      text(data17.getString(0, j).charAt(i), this.x, bottom+25+(n_+3)*i);
     }
   }
 
@@ -299,86 +591,97 @@ class Firework{
   }
 
 
-  //show 2017 / 2018
-  infor(j){
-    if(dist(mouseX,mouseY,right-60,above+10)<10 && j%2==0){     
-     this.alpha = 50;  
-     noStroke();
-     fill(254,82,105);
-     ellipse(right-60,above+13,20,20)
-   }else if(dist(mouseX,mouseY,right-30,above+10)<10 && j%2==1){    
-     this.alpha = 50;
-     noStroke();
-     fill(255,222,85);
-     ellipse(right-30,above+13,20,20);
-   }else if(dist(mouseX,mouseY,right,above+10)<10){    
-     this.alpha = 50;
-     noStroke();
-     fill(255);
-     ellipse(right,above+13,20,20);
+  //show 2017 / 2018  width*0.75+i*(btW+btH*1.2),above/2 + j*(btH+10)
+  infor(j,avg,data){
 
-     if(j%2==1){     
-      if(y[j]<y[j+1]){ //17年小于18年
+
+   if(button == "d"){ 
+     count17 = 0;
+     count18 = 0;
+
+     alpha18 = 50;
+     alpha17 = 50;
+
+     
+
+      if(y17[j]<y18[j]){ //17年小于18年
         fill(255);  
         var r_ = map(windowWidth,0,2000,0,4);
-        var offset = map(y[j+1]-y[j],0,100,0,r_);     
+        var offset = map(y18[j]-y17[j],0,100,0,r_);     
         beginShape();
-        vertex(this.x,y[j]);
-        vertex(this.x-offset,y[j+1]);
-        vertex(this.x+offset,y[j+1]);
+        vertex(this.x,y17[j]);
+        vertex(this.x-offset,y18[j]);
+        vertex(this.x+offset,y18[j]);
         endShape(CLOSE);
-        arc(this.x,y[j+1],offset*2,offset*2,PI*2,PI*3);
+        arc(this.x,y18[j],offset*2,offset*2,PI*2,PI*3);
       }else{//18年小于17年
         fill(255);
         var r_ = map(windowWidth,0,2000,0,4);
-        var offset = map(y[j+1]-y[j],0,100,0,r_);  
+        var offset = map(y18[j]-y17[j],0,100,0,r_);  
         beginShape();
-        vertex(this.x,y[j]);
-        vertex(this.x-offset,y[j+1]);
-        vertex(this.x+offset,y[j+1]);
+        vertex(this.x,y17[j]);
+        vertex(this.x-offset,y18[j]);
+        vertex(this.x+offset,y18[j]);
         endShape(CLOSE);
-        arc(this.x,y[j+1],offset*2,offset*2,PI,PI*2);
+        arc(this.x,y18[j],offset*2,offset*2,PI,PI*2);
       }
-     // line(this.x,y[j],this.x,y[j+1]);
+    } 
+
+    if(button == "b"){    
+      alpha17 = 255;  
+      alpha18 =50;
+      this.tX = map(int((temp18[j]+52)), 1, 50, left, right);
+    }else if(button == "c"){    
+      alpha18 = 255;  
+      alpha17 = 50;  
+      this.tX = map(int((temp17[j]+52)), 1, 50, left, right);
+    }else if(button == "a"){ 
+      this.tX = map(int(j+1), 1, 50, left, right);
+      if(count17%2==1 && count18%2 == 0){       
+       alpha18 = 50;
+       alpha17 = 255;
+     }else if(count18%2==1 && count17%2 == 0){     
+       alpha18 = 255; 
+       alpha17 = 50; 
+     }else if(count18%2==1 && count17%2==1){
+       alpha18 = 255;
+       alpha17 = 255;
+     }else if(count18%2==0 && count17%2==0){
+       alpha18 = 50;
+       alpha17 = 50;
+     }
+
+     if(j==1){
+       policies();
+     }
    }
- }else if(dist(mouseX,mouseY,this.x,this.y)<10){
-  fill(255);
-  textSize(15);
-  text(nfc(avg[j],2),this.x,this.y);
 
-}else{
- this.alpha = 255;
-}
-}
-}
-
-
-
-function check(){
-  noFill();
-  strokeWeight(2);
-  stroke(254,82,105);
-  ellipse(right-60,above+13,20,20);
-  stroke(255,222,85);
-  ellipse(right-30,above+13,20,20);
-  stroke(255);
-  ellipse(right,above+13,20,20);
-  var text17 = "查看2017";
-  var text18 = "查看2018";
-  var compare = "比较17-18";
-  textSize(14);
-  fill(200);
-  noStroke();
-  for (var i = 0; i < text17.length; i++) { 
-    text(text17.charAt(i), right-60,above+40+(15)*i);
-    text(text18.charAt(i), right-30,above+40+(15)*i);
-    text(compare.charAt(i), right,above+40+(15)*i);
+   if(dist(mouseX,mouseY,this.x,this.y)<10){
+    fill(255);
+    textSize(15);
+    text(nfc(avg[j],2),this.x,this.y);
   }
 }
+}
 
 
 
+//排序，但不变化位置
+function selection(tempX, rankX){
+  for(var i = 1; i < cols; i++){
+    var record = -1;
+    var selectedBar = index;
 
+    for(var j = selectedBar; j < cols; j++){
+      if( tempX[j] > record ){
+        selectedBar = j;
+        record = tempX[j];
+      }
+    }
+    tempX[selectedBar] = rankX;//afRank 没有变化位置，只是把value变成rank的值
+    rankX--;
+  }
+}
 
 
 
